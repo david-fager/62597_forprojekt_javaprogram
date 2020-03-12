261429c81a51a975484d905ca31919d7a092ad5f
s185120 <s185120@student.dtu.dk>
2020-03-13 00:15:01 +0100

    ASCII text

--- src/java_server/RunServer.java
+++ src/java_server/RunServer.java
@@ -28,6 +28,8 @@ public class RunServer {
         chrmi = new ConnectionHandlerRMI(chsoap);
         Naming.rebind("rmi://localhost:9920/hangman_local", chrmi);
 
+        RunServer.printASCII();
+
         System.out.println(df.format(Calendar.getInstance().getTimeInMillis()) + "Server started");
 
         serverStatus();
@@ -42,6 +44,15 @@ public class RunServer {
                 chsoap.printAllSessions();
             }
         }, 30*60*1000, 30*60*1000);
+    }
+
+    private static void printASCII() {
+        System.out.println("   ____   ____    _   _   ____    ____    _____     _    ___        ");
+        System.out.println("  / ___| |  _ \\  | | | | |  _ \\  |  _ \\  | ____|   / |  / _ \\   ");
+        System.out.println(" | |  _  | |_) | | | | | | |_) | | |_) | |  _|     | | | | | |      ");
+        System.out.println(" | |_| | |  _ <  | |_| | |  __/  |  __/  | |___    | | | |_| |      ");
+        System.out.println("  \\____| |_| \\_\\  \\___/  |_|     |_|     |_____|   |_|  \\___/  ");
+        System.out.println("                                                                    ");
     }
 
 }
